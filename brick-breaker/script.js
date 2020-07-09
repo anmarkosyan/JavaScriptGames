416f7b1e71488e0e456846afb896539380c0e907
anmarkosyan <anush.markos@gmail.com>
2020-07-09 12:14:48 +0400

    added paddel.js usin module file for call functions from another file

--- brick-breaker/script.js
+++ brick-breaker/script.js
@@ -1,15 +1,14 @@
 //alert("hello")
+import Paddle from "./paddle.js";
+
 
 const canvas = document.getElementById("gameScreen");
 const ctx = canvas.getContext("2d");
-//drew graphics using canvas,in this case rectangles,
-//if you want to add some colors do it first
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
 
 //at first clear rectangle for changed positions
 ctx.clearRect(0,0,800,600);
 
-ctx.fillStyle = "#f00";
-ctx.fillRect(20,20,100,100);
-
-ctx.fillStyle = "#00f"
-ctx.fillRect(370,260,50,50);
+let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+paddle.draw(ctx);
